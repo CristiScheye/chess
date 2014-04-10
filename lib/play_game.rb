775167fad6642b72afcf91550ab896e9fcf7a526
Cristi Scheye <cristi.scheye@gmail.com>
2014-04-09 20:11:02 -0700

    creating a separate file to initialize game

---
+++ lib/play_game.rb
@@ -0,0 +1,19 @@
+# Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file }
+require './chess'
+require './pieces'
+require './board'
+require './player'
+
+if $PROGRAM_NAME == __FILE__
+
+  if ARGV.count == 1
+    Chess.load_game
+  else
+    puts "Enter name for player 1"
+    player1_name = gets.chomp
+    puts "Enter name for player 2"
+    player2_name = gets.chomp
+
+    Chess.new(HumanPlayer.new(player1_name), HumanPlayer.new(player2_name)).play
+  end
+end
